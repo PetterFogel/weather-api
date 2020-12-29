c83e4c58ec31a4709c3e96904de96d4a522514f0
Petter <petter.fogel@medieinstitutet.se>
2020-12-29 18:51:03 +0100

    Add logic to get input value to fetch location.

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-const inputValue = document.getElementById("input-value");
 const submitBtn = document.querySelector(".submit-btn");
-
 
 window.addEventListener("load", main);
 
@@ -12,12 +10,15 @@ function addEventListeners() {
     const form = document.querySelector(".form-container");
     form.addEventListener("submit", (e) => {
         e.preventDefault();
-        fetchYearswithFetch();
+        fetchApiswithFetch();
     });
 }
 
-async function fetchYearswithFetch() {
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=Gothenburg&appid=a55a8434945c72a639d00f4164990b98";
+async function fetchApiswithFetch() {
+    const searchInput = document.getElementById("input-value");
+    const inputValue = searchInput.value;
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=a55a8434945c72a639d00f4164990b98`;
     const result = await fetch(url);
     const data = await result.json();
     console.log(data);
