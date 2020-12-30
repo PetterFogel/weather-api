4be4ebf154eeb58ca9b01b776c63923124b2fea4
Petter <petter.fogel@medieinstitutet.se>
2020-12-30 17:57:15 +0100

    Add logic to calculate celsius from kelvin.

--- script.js
+++ script.js
@@ -23,9 +23,19 @@ async function fetchApiswithFetch() {
     const data = await result.json();
     console.log(data);
     fetchHtmlElements(data);
+    calculateToCelsius(data);
 }
 
 function fetchHtmlElements(data) {
     const cityValue = document.querySelector(".city-value");
-    cityValue.innerHTML = data.name;
+    cityValue.innerText = data.name;
+}
+
+function calculateToCelsius(data) {
+    const tempValue = document.querySelector(".temp-value");
+    let kelvinTemp = data.main.temp;
+
+    let celsiusTemp = Math.round(kelvinTemp - 273.15);
+    tempValue.innerText = celsiusTemp;
+    
 }
